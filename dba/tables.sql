42b69a6826f3529bcaf184d76765c4902585feb5
Miguel Hernandez <migher74@outlook.com>
2024-06-04 19:36:59 -0600

    Games SQL table definition added.

---
+++ dba/tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE games(  
+    gType VARCHAR(1),
+    gWeek VARCHAR(2),
+    gGame INT,
+    gNumber1 INT,
+    gNumber2 INT,
+    gNumber3 INT,
+    gNumber4 INT,
+    gNumber5 INT,
+    gNumber6 INT
+);
