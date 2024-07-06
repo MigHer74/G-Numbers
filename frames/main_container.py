7bd8cb577afd17fa6c9151e608b80b8d50ca9c8a
Miguel Hernandez <migher74@outlook.com>
2024-07-05 20:59:08 -0600

    Labels and entries are inserted into the each game panel to show the latest results.

--- frames/main_container.py
+++ frames/main_container.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Menu
+from tkinter import Tk, Menu, LabelFrame, Label, Entry
 
 
 class Gnumber(Tk):
@@ -10,6 +10,126 @@ class Gnumber(Tk):
     def build(self):
         self.title("G-Numbers - Main Menu")
         self.geometry("600x400")
+
+        self.titlePanel = LabelFrame(self)
+        self.titlePanel.pack()
+
+        self.titleLabel = Label(self.titlePanel, text="Latest Results")
+        self.titleLabel.grid(row=0, column=0)
+
+        self.shortPanel = LabelFrame(self, text=" Short Game Results")
+        self.shortPanel.pack()
+
+        self.shortLabel = Label(self.shortPanel, text="Game #1")
+        self.shortLabel.grid(row=0, column=0)
+
+        self.shortEntry1 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry1.grid(row=0, column=1)
+
+        self.shortEntry2 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry2.grid(row=0, column=2)
+
+        self.shortEntry3 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry3.grid(row=0, column=3)
+
+        self.shortEntry4 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry4.grid(row=0, column=4)
+
+        self.shortEntry5 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry5.grid(row=0, column=5)
+
+        self.shortEntry6 = Entry(self.shortPanel, width=4, justify="center",
+                                 state="readonly")
+        self.shortEntry6.grid(row=0, column=6)
+
+        self.longPanel = LabelFrame(self, text=" Long Game Results")
+        self.longPanel.pack()
+
+        self.longLabel1 = Label(self.longPanel, text="Game #1")
+        self.longLabel1.grid(row=0, column=0)
+
+        self.longEntry11 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry11.grid(row=0, column=1)
+
+        self.longEntry12 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry12.grid(row=0, column=2)
+
+        self.longEntry13 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry13.grid(row=0, column=3)
+
+        self.longEntry14 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry14.grid(row=0, column=4)
+
+        self.longEntry15 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry15.grid(row=0, column=5)
+
+        self.longEntry16 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry16.grid(row=0, column=6)
+
+        self.longLabel2 = Label(self.longPanel, text="Game #2")
+        self.longLabel2.grid(row=1, column=0)
+
+        self.longEntry21 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry21.grid(row=1, column=1)
+
+        self.longEntry22 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry22.grid(row=1, column=2)
+
+        self.longEntry23 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry23.grid(row=1, column=3)
+
+        self.longEntry24 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry24.grid(row=1, column=4)
+
+        self.longEntry25 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry25.grid(row=1, column=5)
+
+        self.longEntry26 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry26.grid(row=1, column=6)
+
+        self.longLabel3 = Label(self.longPanel, text="Game #3")
+        self.longLabel3.grid(row=2, column=0)
+
+        self.longEntry31 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry31.grid(row=2, column=1)
+
+        self.longEntry32 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry32.grid(row=2, column=2)
+
+        self.longEntry33 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry33.grid(row=2, column=3)
+
+        self.longEntry34 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry34.grid(row=2, column=4)
+
+        self.longEntry35 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry35.grid(row=2, column=5)
+
+        self.longEntry36 = Entry(self.longPanel, width=4, justify="center",
+                                 state="readonly")
+        self.longEntry36.grid(row=2, column=6)
 
     def menu(self):
         self.mainMenu = Menu()
