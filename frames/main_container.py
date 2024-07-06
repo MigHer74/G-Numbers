5f27d2682a4f61a041f673987410b97b88a28431
Miguel Hernandez <migher74@outlook.com>
2024-07-05 21:55:31 -0600

    Applied style on title panel, short game panel and long game panel.

--- frames/main_container.py
+++ frames/main_container.py
@@ -10,126 +10,131 @@ class Gnumber(Tk):
     def build(self):
         self.title("G-Numbers - Main Menu")
         self.geometry("600x400")
+        self.resizable(False, False)
 
         self.titlePanel = LabelFrame(self)
-        self.titlePanel.pack()
+        self.titlePanel.pack(pady=(37, 0))
 
-        self.titleLabel = Label(self.titlePanel, text="Latest Results")
+        self.titleLabel = Label(self.titlePanel, width=34, height=2,
+                                text="Latest Results",
+                                font=("Arial", 12, "bold"))
         self.titleLabel.grid(row=0, column=0)
 
-        self.shortPanel = LabelFrame(self, text=" Short Game Results")
-        self.shortPanel.pack()
+        self.shortPanel = LabelFrame(self, text=" Short Game Results",
+                                     labelanchor="n")
+        self.shortPanel.pack(pady=(30, 0))
 
         self.shortLabel = Label(self.shortPanel, text="Game #1")
-        self.shortLabel.grid(row=0, column=0)
+        self.shortLabel.grid(row=0, column=0, padx=(15, 5), pady=15)
 
         self.shortEntry1 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry1.grid(row=0, column=1)
+        self.shortEntry1.grid(row=0, column=1, padx=(15, 0), pady=15)
 
         self.shortEntry2 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry2.grid(row=0, column=2)
+        self.shortEntry2.grid(row=0, column=2, padx=(15, 0), pady=15)
 
         self.shortEntry3 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry3.grid(row=0, column=3)
+        self.shortEntry3.grid(row=0, column=3, padx=(15, 0), pady=15)
 
         self.shortEntry4 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry4.grid(row=0, column=4)
+        self.shortEntry4.grid(row=0, column=4, padx=(15, 0), pady=15)
 
         self.shortEntry5 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry5.grid(row=0, column=5)
+        self.shortEntry5.grid(row=0, column=5, padx=(15, 0), pady=15)
 
         self.shortEntry6 = Entry(self.shortPanel, width=4, justify="center",
                                  state="readonly")
-        self.shortEntry6.grid(row=0, column=6)
+        self.shortEntry6.grid(row=0, column=6, padx=(15, 15), pady=15)
 
-        self.longPanel = LabelFrame(self, text=" Long Game Results")
-        self.longPanel.pack()
+        self.longPanel = LabelFrame(self, text=" Long Game Results",
+                                    labelanchor="n")
+        self.longPanel.pack(pady=(30, 0))
 
         self.longLabel1 = Label(self.longPanel, text="Game #1")
-        self.longLabel1.grid(row=0, column=0)
+        self.longLabel1.grid(row=0, column=0, padx=(15, 5), pady=(15, 0))
 
         self.longEntry11 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry11.grid(row=0, column=1)
+        self.longEntry11.grid(row=0, column=1, padx=(15, 0), pady=(15, 0))
 
         self.longEntry12 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry12.grid(row=0, column=2)
+        self.longEntry12.grid(row=0, column=2, padx=(15, 0), pady=(15, 0))
 
         self.longEntry13 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry13.grid(row=0, column=3)
+        self.longEntry13.grid(row=0, column=3, padx=(15, 0), pady=(15, 0))
 
         self.longEntry14 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry14.grid(row=0, column=4)
+        self.longEntry14.grid(row=0, column=4, padx=(15, 0), pady=(15, 0))
 
         self.longEntry15 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry15.grid(row=0, column=5)
+        self.longEntry15.grid(row=0, column=5, padx=(15, 0), pady=(15, 0))
 
         self.longEntry16 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry16.grid(row=0, column=6)
+        self.longEntry16.grid(row=0, column=6, padx=(15, 15), pady=(15, 0))
 
         self.longLabel2 = Label(self.longPanel, text="Game #2")
-        self.longLabel2.grid(row=1, column=0)
+        self.longLabel2.grid(row=1, column=0, padx=(15, 5), pady=(15, 0))
 
         self.longEntry21 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry21.grid(row=1, column=1)
+        self.longEntry21.grid(row=1, column=1, padx=(15, 0), pady=(15, 0))
 
         self.longEntry22 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry22.grid(row=1, column=2)
+        self.longEntry22.grid(row=1, column=2, padx=(15, 0), pady=(15, 0))
 
         self.longEntry23 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry23.grid(row=1, column=3)
+        self.longEntry23.grid(row=1, column=3, padx=(15, 0), pady=(15, 0))
 
         self.longEntry24 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry24.grid(row=1, column=4)
+        self.longEntry24.grid(row=1, column=4, padx=(15, 0), pady=(15, 0))
 
         self.longEntry25 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry25.grid(row=1, column=5)
+        self.longEntry25.grid(row=1, column=5, padx=(15, 0), pady=(15, 0))
 
         self.longEntry26 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry26.grid(row=1, column=6)
+        self.longEntry26.grid(row=1, column=6, padx=(15, 15), pady=(15, 0))
 
         self.longLabel3 = Label(self.longPanel, text="Game #3")
-        self.longLabel3.grid(row=2, column=0)
+        self.longLabel3.grid(row=2, column=0, padx=(15, 5), pady=15)
 
         self.longEntry31 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry31.grid(row=2, column=1)
+        self.longEntry31.grid(row=2, column=1, padx=(15, 0), pady=15)
 
         self.longEntry32 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry32.grid(row=2, column=2)
+        self.longEntry32.grid(row=2, column=2, padx=(15, 0), pady=15)
 
         self.longEntry33 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry33.grid(row=2, column=3)
+        self.longEntry33.grid(row=2, column=3, padx=(15, 0), pady=15)
 
         self.longEntry34 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry34.grid(row=2, column=4)
+        self.longEntry34.grid(row=2, column=4, padx=(15, 0), pady=15)
 
         self.longEntry35 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry35.grid(row=2, column=5)
+        self.longEntry35.grid(row=2, column=5, padx=(15, 0), pady=15)
 
         self.longEntry36 = Entry(self.longPanel, width=4, justify="center",
                                  state="readonly")
-        self.longEntry36.grid(row=2, column=6)
+        self.longEntry36.grid(row=2, column=6, padx=(15, 15), pady=15)
 
     def menu(self):
         self.mainMenu = Menu()
