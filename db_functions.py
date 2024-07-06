d9cefbb7bbe0c23d1c5ffc5f0cd864305484a7fa
Miguel Hernandez <migher74@outlook.com>
2024-07-05 22:19:35 -0600

    Function to retrieve all the numbers of the latest results.

--- db_functions.py
+++ db_functions.py
@@ -4,3 +4,13 @@ import sqlite3
 def dbaConnection():
     dbaConnect = sqlite3.connect("db/G-Numbers.db")
     return dbaConnect
+
+
+def dbaShowLatest():
+    dbcon = dbaConnection()
+    dbcur = dbcon.cursor()
+    dbdatas = dbcur.execute("SELECT * FROM 'games' WHERE gType = 'S'\
+                            AND gWeek = 'W1'").fetchone()
+    dbdatal = dbcur.execute("SELECT * FROM 'games' WHERE gType = 'L'\
+                            AND gWeek = 'W1'").fetchall()
+    return dbdatas, dbdatal
