892a44e7377f1df6104477db731e1d22908c1e2f
Miguel Hernandez <migher74@outlook.com>
2024-07-15 22:19:12 -0600

    Function to update the game data depending on the game type to prepare for the new game numbers of the week.

--- db_functions.py
+++ db_functions.py
@@ -14,3 +14,29 @@ def dbaShowLatest():
     dbdatal = dbcur.execute("SELECT * FROM 'games' WHERE gType = 'L'\
                             AND gWeek = 'W1'").fetchall()
     return dbdatas, dbdatal
+
+
+def dbaUpdateGame(dbtype):
+    dblist = ["W6", "W5", "W4", "W3", "W2", "W1"]
+
+    if dbtype == "short":
+        dbtype = "S"
+    else:
+        dbtype = "L"
+
+    dbcon = dbaConnection()
+    dbcur = dbcon.cursor()
+
+    row1 = "DELETE FROM games WHERE gWeek ='W6'"
+    row2 = f"AND gType = '{dbtype}';"
+    command = row1 + row2
+    dbcur.execute(command)
+
+    for dbindex in range(1, 6):
+        row1 = f"UPDATE games SET gWeek = '{dblist[dbindex-1]}'"
+        row2 = f"WHERE gWeek = '{dblist[dbindex]}' AND gType = '{dbtype}';"
+        command = row1 + row2
+        dbcur.execute(command)
+
+    dbcon.commit()
+    dbcon.close()
