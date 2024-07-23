1b74cfb5a65e3d332cdc9465d3b49ad97867db20
Miguel Hernandez <migher74@outlook.com>
2024-07-22 20:54:37 -0600

    Add method to get results depending on the game selected.

--- frames/regame_container.py
+++ frames/regame_container.py
@@ -1,5 +1,6 @@
-from tkinter import Toplevel, Frame, LabelFrame, Radiobutton, Button
+from tkinter import Toplevel, Frame, LabelFrame, Radiobutton, Button, Label
 from tkinter import StringVar
+import game_engine as gen
 
 
 class RGPanel(Toplevel):
@@ -26,11 +27,13 @@ class RGPanel(Toplevel):
         self.replayGame.grid(row=0, column=0)
 
         self.replayShort = Radiobutton(self.replayGame, text="Short Game",
-                                       value="short", variable=self.replayType)
+                                       value="short", variable=self.replayType,
+                                       command=self.actionGame)
         self.replayShort.grid(row=0, column=0, padx=(10, 10), pady=(10, 0))
 
         self.replayLong = Radiobutton(self.replayGame, text="Long Game",
-                                      value="long", variable=self.replayType)
+                                      value="long", variable=self.replayType,
+                                      command=self.actionGame)
         self.replayLong.grid(row=1, column=0, padx=(10, 10), pady=(10, 10))
 
         self.panelTwo = Frame(self)
@@ -52,3 +55,38 @@ class RGPanel(Toplevel):
                                        labelanchor="n")
         self.panelResults.grid(row=0, column=0, padx=(15, 15), pady=(15, 0))
         self.panelResults.grid_propagate(False)
+
+    def actionGame(self):
+        self.replayShort.config(state="disabled")
+        self.replayLong.config(state="disabled")
+        self.newButton.config(state="normal")
+
+        (listResult1, listResult2,
+         listResult3, listResult4) = gen.gEngine(self.replayType.get())
+
+        if self.replayType.get() == "short":
+            result1 = f"5+ -> {", ".join(listResult1)}"
+            result2 = f"4 --> {", ".join(listResult2)}"
+            result3 = f"3 --> {", ".join(listResult3)}"
+            result4 = f"2 --> {", ".join(listResult4)}"
+        else:
+            result1 = f"6+ -> {", ".join(listResult1)}"
+            result2 = f"5 --> {", ".join(listResult2)}"
+            result3 = f"4 --> {", ".join(listResult3)}"
+            result4 = f"3 --> {", ".join(listResult4)}"
+
+        self.labelRow01 = Label(self.panelResults, text=result1)
+        self.labelRow01.grid(row=0, column=0, padx=10, pady=(5, 0),
+                             sticky="w")
+
+        self.labelRow02 = Label(self.panelResults, text=result2)
+        self.labelRow02.grid(row=1, column=0, padx=10, pady=(10, 0),
+                             sticky="w")
+
+        self.labelRow03 = Label(self.panelResults, text=result3)
+        self.labelRow03.grid(row=2, column=0, padx=10, pady=(10, 0),
+                             sticky="w")
+
+        self.labelRow04 = Label(self.panelResults, text=result4)
+        self.labelRow04.grid(row=3, column=0, padx=10, pady=(10, 10),
+                             sticky="w")
