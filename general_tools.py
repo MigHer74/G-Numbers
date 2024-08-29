eaa9657794aa17af5ae6fe37fbd9d8bb2689d30f
Miguel Hernandez <migher74@outlook.com>
2024-08-28 18:04:57 -0600

    Function to get the values for each week of long game and convert the list data into a string.

--- general_tools.py
+++ general_tools.py
@@ -11,3 +11,17 @@ def verify_data():
 
     if not Path(dbFile).is_file():
         dba.dbaCreateDatabase()
+
+
+def load_weeks(loadtype, loadweek):
+    data = dba.dbaRetrieveOrderValues(loadtype, loadweek)
+
+    temp_game1 = data[0][3:]
+    temp_game2 = data[1][3:]
+    temp_game3 = data[2][3:]
+
+    temp_game1 = ", ".join(map(str, temp_game1))
+    temp_game2 = ", ".join(map(str, temp_game2))
+    temp_game3 = ", ".join(map(str, temp_game3))
+
+    return temp_game1, temp_game2, temp_game3
