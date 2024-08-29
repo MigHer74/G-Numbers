aa7bf22da98b2a0246bc6e74c616a5b3ac9b917c
Miguel Hernandez <migher74@outlook.com>
2024-08-28 20:05:42 -0600

    Method to get the data from the database from each week of all long games and initiate the new design of the panels to show the results of previous games.

--- frames/lview_container.py
+++ frames/lview_container.py
@@ -1,5 +1,6 @@
 from tkinter import Toplevel, Frame, LabelFrame, Entry, Label, Button
 from tkinter import Radiobutton
+import general_tools as gt
 
 
 class LongView(Toplevel):
@@ -7,7 +8,9 @@ class LongView(Toplevel):
         super().__init__()
         self.transient(parent)
         self.grab_set()
+        self.loadData()
         self.longBuild()
+        # self.updateLabels()
 
     def longBuild(self):
         self.title("G-Numbers | Data Long Game")
@@ -17,88 +20,51 @@ class LongView(Toplevel):
         self.lblFrame1.grid(row=0, column=0, padx=(15, 0), pady=(15, 0))
 
         self.radioOne = Radiobutton(self.lblFrame1, text="Week #1")
-        self.radioOne.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
+        self.radioOne.grid(row=0, column=0, pady=(10, 0))
 
-        self.label11 = Label(self.lblFrame1, text="Game #1:")
-        self.label11.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
+        Label(self.lblFrame1, text=self.label11).grid(row=1, column=0, padx=(10, 10), pady=(10, 0), sticky="w")
+        Label(self.lblFrame1, text=self.label12).grid(row=2, column=0, padx=(10, 10), pady=(10, 0), sticky="w")
+        Label(self.lblFrame1, text=self.label13).grid(row=3, column=0, padx=(10, 10), pady=(10, 10),sticky="w")
 
-        self.label12 = Label(self.lblFrame1, text="Game #2:")
-        self.label12.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
+    def loadData(self):
+        (self.week1Game1, self.week1Game2,
+         self.week1Game3) = gt.load_weeks("long", "W1")
 
-        self.label13 = Label(self.lblFrame1, text="Game #3:")
-        self.label13.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
+        (self.week2Game1, self.week2Game2,
+         self.week2Game3) = gt.load_weeks("long", "W2")
 
-        self.lblFrame2 = LabelFrame(self)
-        self.lblFrame2.grid(row=0, column=1, padx=(15, 0), pady=(15, 0))
+        (self.week3Game1, self.week3Game2,
+         self.week3Game3) = gt.load_weeks("long", "W3")
 
-        self.radio2 = Radiobutton(self.lblFrame2, text="Week #2")
-        self.radio2.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
+        (self.week4Game1, self.week4Game2,
+         self.week4Game3) = gt.load_weeks("long", "W4")
 
-        self.label21 = Label(self.lblFrame2, text="Game #1:")
-        self.label21.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
+        (self.week5Game1, self.week5Game2,
+         self.week5Game3) = gt.load_weeks("long", "W5")
 
-        self.label22 = Label(self.lblFrame2, text="Game #2:")
-        self.label22.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
+        (self.week6Game1, self.week6Game2,
+         self.week6Game3) = gt.load_weeks("long", "W6")
 
-        self.label23 = Label(self.lblFrame2, text="Game #3:")
-        self.label23.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
+        self.label11 = f"Game #1  --->  {self.week1Game1}"
+        self.label12 = f"Game #2  --->  {self.week1Game2}"
+        self.label13 = f"Game #3  --->  {self.week1Game3}"
 
-        self.lblFrame3 = LabelFrame(self)
-        self.lblFrame3.grid(row=0, column=2, padx=(15, 15), pady=(15, 0))
+        self.label21 = f"Game #1  --->  {self.week2Game1}"
+        self.label22 = f"Game #2  --->  {self.week2Game2}"
+        self.label23 = f"Game #3  --->  {self.week2Game3}"
 
-        self.radio3 = Radiobutton(self.lblFrame3, text="Week #3")
-        self.radio3.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
+        self.label31 = f"Game #1  --->  {self.week3Game1}"
+        self.label32 = f"Game #2  --->  {self.week3Game2}"
+        self.label33 = f"Game #3  --->  {self.week3Game3}"
 
-        self.label31 = Label(self.lblFrame3, text="Game #1:")
-        self.label31.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
+        self.label41 = f"Game #1  --->  {self.week4Game1}"
+        self.label42 = f"Game #2  --->  {self.week4Game2}"
+        self.label43 = f"Game #3  --->  {self.week4Game3}"
 
-        self.label32 = Label(self.lblFrame3, text="Game #2:")
-        self.label32.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
+        self.label51 = f"Game #1  --->  {self.week5Game1}"
+        self.label52 = f"Game #2  --->  {self.week5Game2}"
+        self.label53 = f"Game #3  --->  {self.week5Game3}"
 
-        self.label33 = Label(self.lblFrame3, text="Game #3:")
-        self.label33.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
-
-        self.lblFrame4 = LabelFrame(self)
-        self.lblFrame4.grid(row=1, column=0, padx=(15, 0), pady=(15, 15))
-
-        self.radio4 = Radiobutton(self.lblFrame4, text="Week #4")
-        self.radio4.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
-
-        self.label41 = Label(self.lblFrame4, text="Game #1:")
-        self.label41.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label42 = Label(self.lblFrame4, text="Game #2:")
-        self.label42.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label43 = Label(self.lblFrame4, text="Game #3:")
-        self.label43.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
-
-        self.lblFrame5 = LabelFrame(self)
-        self.lblFrame5.grid(row=1, column=1, padx=(15, 0), pady=(15, 15))
-
-        self.radio5 = Radiobutton(self.lblFrame5, text="Week #5")
-        self.radio5.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
-
-        self.label51 = Label(self.lblFrame5, text="Game #1:")
-        self.label51.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label52 = Label(self.lblFrame5, text="Game #2:")
-        self.label52.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label53 = Label(self.lblFrame5, text="Game #3:")
-        self.label53.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
-
-        self.lblFrame6 = LabelFrame(self)
-        self.lblFrame6.grid(row=1, column=2, padx=(15, 15), pady=(15, 15))
-
-        self.radio6 = Radiobutton(self.lblFrame6, text="Week #6")
-        self.radio6.grid(row=0, column=0, padx=(15, 0), pady=(10, 0))
-
-        self.label61 = Label(self.lblFrame6, text="Game #1:")
-        self.label61.grid(row=1, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label62 = Label(self.lblFrame6, text="Game #2:")
-        self.label62.grid(row=2, column=0, padx=(38, 0), pady=(10, 0))
-
-        self.label63 = Label(self.lblFrame6, text="Game #3:")
-        self.label63.grid(row=3, column=0, padx=(38, 0), pady=(10, 10))
+        self.label61 = f"Game #1  --->  {self.week6Game1}"
+        self.label62 = f"Game #2  --->  {self.week6Game2}"
+        self.label63 = f"Game #3  --->  {self.week6Game3}"
