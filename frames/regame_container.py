10cc3330cbe18330e5849cd4305a62f6048b6c8a
Miguel Hernandez <migher74@outlook.com>
2025-07-06 23:01:02 -0600

    Remove the centerFrame method and center the window in the build method using the center_window function.

--- frames/regame_container.py
+++ frames/regame_container.py
@@ -1,5 +1,6 @@
 from tkinter import Toplevel, Frame, LabelFrame, Radiobutton, Button, Label
 from tkinter import StringVar
+from general_tools import center_window
 import game_engine as gen
 
 
@@ -13,11 +14,11 @@ class RGPanel(Toplevel):
         self.replayType.set(None)
 
         self.build()
-        self.centerFrame()
         self.focus()
 
     def build(self):
         self.title("G-Numbers | Replay Game")
+        center_window(self, 479, 231)
         self.resizable(False, False)
 
         self.panelOne = Frame(self)
@@ -103,13 +104,3 @@ class RGPanel(Toplevel):
         self.labelRow02.destroy()
         self.labelRow03.destroy()
         self.labelRow04.destroy()
-
-    def centerFrame(self):
-        self.update()
-        w = self.winfo_width()
-        h = self.winfo_height()
-        ws = self.winfo_screenwidth()
-        hs = self.winfo_screenheight()
-        x = int(ws/2 - w/2)
-        y = int(hs/2 - h/2)
-        self.geometry(f"{w}x{h}+{x}+{y}")
