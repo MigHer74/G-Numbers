0f70b93bb2ea083a3ea7fe5fc02b69242616fd06
Miguel Hernandez <migher74@outlook.com>
2025-07-21 22:58:01 -0600

    Add the final_values function to return the final 6 numbers. If the final list of values is less than 6, the code will select a random number from the list with the most numbers.

--- game_engine.py
+++ game_engine.py
@@ -1,3 +1,4 @@
+import random as rdm
 import db_functions as dba
 
 
@@ -46,4 +47,29 @@ def gEngine(gameType):
 
             choice = 0
 
-    return rowResult1, rowResult2, rowResult3, rowResult4
+            rowFinal = final_values(rowResult1, rowResult2, rowResult3,
+                                    rowResult4)
+
+    return rowResult1, rowResult2, rowResult3, rowResult4, rowFinal
+
+
+def final_values(row1, row2, row3, row4):
+    final_list = []
+    temp_list = []
+
+    for row in [row1, row2, row3, row4]:
+        if len(final_list) < 6:
+            if len(row) != 0 and len(row) <= 6:
+                for number in row:
+                    if len(final_list) < 6:
+                        final_list.append(number)
+                    else:
+                        break
+            elif len(row) > 6:
+                temp_list = row.copy()
+                for item in range(6 - len(final_list)):
+                    selection = rdm.choice(temp_list)
+                    final_list.append(selection)
+                    temp_list.remove(selection)
+
+    return final_list
